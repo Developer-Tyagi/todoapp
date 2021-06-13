46c8a2a39e638d182131ee4f528ca2c1d68ffd47
tya6i <developertyagi1203@gmail.com>
2021-06-13 18:44:43 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, {useState} from "react"
+import './App.css';
+
+const App = ()=> {
+  const [inputList, setInputList] = useState("");
+  const [Items, setItems] = useState([]);
+
+  const itemChange= (event) => {
+      setInputList(event.target.value);
+  }
+
+  const listOfItems = () =>{
+    setItems((oldItems) => {
+      return[...oldItems,inputList];
+    });
+      setInputList("");
+  };
+
+  return (
+    <div className="main_div">
+      <div className="centre_div">
+         <br />
+         <h1>ToDo List</h1>
+         <br />
+         <input type="text" placeholder="Add a items" value={inputList}  onChange= {itemChange} ></input>
+         <button onClick={listOfItems}> +</button>
+
+           <ol>
+              {/* <li>{inputList}</li> */}
+
+              {Items.map( (value) => {
+                return<li>{value}</li>;
+              })}
+
+             </ol>      
+     </div>
+    </div>
+  );
+}
+
+export default App;
