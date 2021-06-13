bde392d5d5d7034526f08335ddc6708501edf739
tya6i <developertyagi1203@gmail.com>
2021-06-13 19:25:48 +0530

    code push

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
-import React, {useState} from "react"
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-const App = ()=> {
+const App = () => {
   const [inputList, setInputList] = useState("");
   const [Items, setItems] = useState([]);
 
-  const itemChange= (event) => {
-      setInputList(event.target.value);
-  }
+  const itemChange = (event) => {
+    setInputList(event.target.value);
+  };
 
-  const listOfItems = () =>{
+  const listOfItems = () => {
     setItems((oldItems) => {
-      return[...oldItems,inputList];
+      return [...oldItems, inputList];
     });
-      setInputList("");
+    setInputList("");
   };
 
   return (
     <div className="main_div">
       <div className="centre_div">
-         <br />
-         <h1>ToDo List</h1>
-         <br />
-         <input type="text" placeholder="Add a items" value={inputList}  onChange= {itemChange} ></input>
-         <button onClick={listOfItems}> +</button>
+        <br />
+        <h1>ToDo List</h1>
+        <br />
+        <input
+          type="text"
+          placeholder="Add a items"
+          value={inputList}
+          onChange={itemChange}
+        ></input>
+        <button onClick={listOfItems}> +</button>
 
-           <ol>
-              {/* <li>{inputList}</li> */}
+        <ol>
+          {/* <li>{inputList}</li> */}
 
-              {Items.map( (value) => {
-                return<li>{value}</li>;
-              })}
-
-             </ol>      
-     </div>
+          {Items.map((value) => {
+            return <li>{value}</li>;
+          })}
+        </ol>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
